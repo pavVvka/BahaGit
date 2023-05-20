d59a2a3616118b28138fe732acac470afe8075ab
Pavel <pavvvka@gmail.com>
2023-05-20 18:29:46 +0300

    st2: added POM file and 2nd sout in Main file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("1step: my first repo!");
+        System.out.println("2step: this new sout");
     }
 }
